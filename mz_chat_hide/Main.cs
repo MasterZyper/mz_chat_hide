ecbc14f4b26da2c31bbac47c594058868d93932e
MaserZyper <masterzyper@reloadd-server.de>
2019-10-03 16:49:49 +0200

    Projektdateien hinzufügen.

---
+++ mz_chat_hide/Main.cs
@@ -0,0 +1,24 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mz_chat_hide
+{
+    public class Main : BaseScript
+    {
+        public Main() 
+        {
+            Tick += EachFrame;        
+        }
+        private async Task EachFrame() 
+        {
+            API.SetTextChatEnabled(false);
+
+            await Delay(0);        
+        }
+    }
+}
---
+++ mz_chat_hide/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mz_chat_hide")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mz_chat_hide")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6d13bee-a629-4b09-9b5f-2302de61abd7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
